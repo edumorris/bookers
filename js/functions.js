9488418ef0b65c51bf01bdf466b1598e0b51e71f
edumorris <eduuwachira@gmail.com>
2020-04-12 14:14:29 +0300

    Responsive update

--- js/functions.js
+++ js/functions.js
@@ -21,3 +21,15 @@ function closeMenu() {
 function shopMessage {
 	confirm("This site is underconstruction and will be available soon");
 }
+
+function openMenuHome() {
+	document.getElementById("menu").style.display = "block";
+	document.getElementById("menu-btn-home").style.display = "none";
+	document.getElementById("close-btn-home").style.display = "block";
+}
+
+function closeMenuHome() {
+	document.getElementById("menu").style.display = "none";
+	document.getElementById("menu-btn-home").style.display = "block";
+	document.getElementById("close-btn-home").style.display = "none";
+}
